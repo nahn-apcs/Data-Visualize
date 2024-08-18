5f417d081c43c8947a8c51ee30cc8bd59d548741
nahn-apcs <kenpro030709@gmail.com>
2024-08-18 13:01:11 +0700

    full core of heap

--- include/screen/HeapScreen.hpp
+++ include/screen/HeapScreen.hpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <vector>
 
+
 class HeapScreen 
 {
 public:
@@ -51,30 +52,54 @@ private:
         int pos_x, pos_y, id, val;
         int vt;
         float per_node, per_edge_l, per_edge_r;
+        float per_next;//độ đậm nếu đó là nút next
         bool now, next; //nut hien tai, nut tiep theo
 
-        node (int _pos_x = 0, int _pos_y = 0, int _val = 0): pos_x(_pos_x), pos_y(_pos_y), val(_val), per_node(0), per_edge_l(0), per_edge_r(0), now(0), next(0) {}
+        node (int _pos_x = 0, int _pos_y = 0, int _val = 0): pos_x(_pos_x), pos_y(_pos_y), val(_val), per_node(0), per_edge_l(0), per_edge_r(0), per_next(0), now(0), next(0) {}
     };
     std::vector<node>ds_node;
-    std::vector<std::pair<float, float>>cur_posisition;
     int time_vi;
 
 
     void reset_heap();
     void insert_heap(int val);
-    void dfs_inoder(int u);
-    void set_position();
-    void draw_node(int u, std::vector<node>&);
+    void dfs_inoder(int u, int block);
+    void set_ds_node();
+    void draw_node(int u, std::vector<node>&, std::vector<node>&);
     void up_heapify(int u);
     void animation_shift_tree_new_node(float time);
-    void draw_heap(std::vector<node>&);
+    void draw_heap(std::vector<node>& tree_stage, std::vector<node>& postition_stage);
     void animation_swap_node(int cha, int u, float time);
 
 
 //animation
 private:
     std::vector<std::vector<node>>tree_state;
+    std::vector<std::vector<node>>postition_state;
     int cur_stage = 0;
+
+
+//timebar
+private:
+    void draw_time_bar();
+
+
+//extract_max_function
+private:
+    void extract_max();
+    void delete_node(int u);
+    void animation_shift_tree_delete_node(float time);
+    void set_ds_node_delete();
+    void down_heapify(int u);
+
+
+//update_function
+private:
+    void update_pos(int vt, int val);
+
+//delete_function
+private:
+    void delete_pos(int vt);
 };
 
 
--- src/screen/HeapScreen.cpp
+++ src/screen/HeapScreen.cpp
@@ -27,35 +27,38 @@ void HeapScreen::run() {
 
     reset_heap();
 
-    insert_heap(20);
-    insert_heap(30);
-    insert_heap(40);
+    insert_heap(10);
 
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);
 
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);
-    insert_heap(40);
-    insert_heap(40);
-    insert_heap(99);
-    insert_heap(99);    
+    insert_heap(11);
+    insert_heap(12);
+    insert_heap(13);
+
+    insert_heap(14);
+    insert_heap(15);
+    insert_heap(15);
+
+    insert_heap(14);
+    insert_heap(15);
+    insert_heap(15);
+
+    insert_heap(14);
+    insert_heap(15);
+    insert_heap(15);
+
+    insert_heap(14);
+    insert_heap(15);
+    insert_heap(15);
+
+   
+
+    extract_max();
+
+    update_pos(3, 99);
+
+    delete_pos(8);
+
+
 
     cur_stage = 0;
 
@@ -121,28 +124,48 @@ void HeapScreen::Render() {
     mWindow.clear();
     render_image();
     cur_stage = std::min(cur_stage, (int)tree_state.size() - 1);
-    draw_heap(tree_state[cur_stage]);
-
-
+    if(cur_stage >= 0 && cur_stage < tree_state.size()) {
+        draw_heap(tree_state[cur_stage], postition_state[cur_stage]);
+        draw_time_bar();
+    }
 
     mWindow.display();
     cur_stage = cur_stage + is_play;
 }
 
+void HeapScreen::draw_time_bar() {
+
+
+    //rgb(116,255,188)
+    float per = 1.0 * (cur_stage + 1) / (int)tree_state.size();
+
+    sf::RectangleShape Line;
+
+    Line.setSize(sf::Vector2f(500.0f * per, 15));
+    Line.setFillColor(sf::Color(116, 225, 188));
+
+    // Đặt vị trí và góc cho hình chữ nhật
+    Line.setPosition(468, 864);
+
+    mWindow.draw(Line);
+
+}
+
 void HeapScreen::reset_heap() {
     ds_node.clear();
     tree_state.clear();
+    postition_state.clear();
 }
 
-void HeapScreen::dfs_inoder(int u) {
+void HeapScreen::dfs_inoder(int u, int block) {
     int v = 2 * u;
     if(v <= ds_node.size()) {
-        dfs_inoder(v);
+        dfs_inoder(v, block);
     }
-    ds_node[u - 1].vt = ++time_vi;
+    if(u != block) ds_node[u - 1].vt = ++time_vi;
     v = 2 * u + 1;
     if(v <= ds_node.size()) {
-        dfs_inoder(v);
+        dfs_inoder(v, block);
     }
 }
 
@@ -150,14 +173,13 @@ void HeapScreen::insert_heap(int val) {
     node tmp(0, 0, val);
     ds_node.push_back(tmp);
 
-
     time_vi = 0;
 
     if(ds_node.size()) {
-        dfs_inoder(1);
+        dfs_inoder(1, 0);
 
 
-        animation_shift_tree_new_node(0.5f);
+        animation_shift_tree_new_node(0.35f);
 
         up_heapify(ds_node.size());
     }
@@ -165,11 +187,12 @@ void HeapScreen::insert_heap(int val) {
 
 void HeapScreen::animation_shift_tree_new_node(float time) {
 
-    for(int i = 0; i < ds.size(); i++) ds_node[i].now = ds_node[i].next = 0;
+    for(int i = 0; i < ds_node.size(); i++) ds_node[i].now = ds_node[i].next = 0;
     if(ds_node.size()) ds_node.back().now = 1;
 
     //tree_state.clear();
-    tree_state.push_back(ds_node);
+    //tree_state.push_back(ds_node);
+    //postition_state.push_back(ds_node);
 
     for(int t = 1; t <= 60 * time; t++) {
         std::vector<node>tmp;
@@ -229,22 +252,30 @@ void HeapScreen::animation_shift_tree_new_node(float time) {
         }
 
         tree_state.push_back(tmp);
+        postition_state.push_back(tmp);
     }
 
 
-    set_position();
+    set_ds_node();
+    tree_state.push_back(ds_node);
+    postition_state.push_back(ds_node);
 }
 
 void HeapScreen::up_heapify(int u) {
     int cha = u / 2;
     if(cha < 1) return;
+
+    for(int i = 0; i < ds_node.size(); i++) {
+        ds_node[i].now = ds_node[i].next = 0;
+    }
+    ds_node[u - 1].now = 1;
+
+
+
     if(ds_node[cha - 1].val < ds_node[u - 1].val) {
 
 
-        animation_swap_node(cha, u, 0.5f);
-        std::swap(ds_node[cha - 1].val, ds_node[u - 1].val);
-
-        tree_state.push_back(ds_node);
+        animation_swap_node(cha, u, 0.35f);
         up_heapify(u / 2);
     }
     return;
@@ -258,11 +289,29 @@ void HeapScreen::animation_swap_node(int cha, int u, float time) {
     ds_node[u - 1].now = 1;
     ds_node[cha - 1].next = 1;
 
+
     tree_state.push_back(ds_node);
+    postition_state.push_back(ds_node);
+
+
+    for(int t = 1; t <= 30 * time; t++) {
+        std::vector<node>tmp;
+        for(int i = 0; i < ds_node.size(); i++) {
+            tmp.push_back(ds_node[i]);
+            if(i == cha - 1) {
+                tmp.back().per_next = t / (30 * time);
+            }
+        }
+        tree_state.push_back(tmp);
+        postition_state.push_back(ds_node);
+    }
+
+    ds_node[cha - 1].per_next = 1;
 
     for(int t = 1; t <= 60 * time; t++) {
         std::vector<node>tmp;
 
+        postition_state.push_back(ds_node);
         for(int i = 0; i < ds_node.size(); i++) {
             tmp.push_back(ds_node[i]);
             if(i + 1 == u) {
@@ -282,9 +331,41 @@ void HeapScreen::animation_swap_node(int cha, int u, float time) {
         }
         tree_state.push_back(tmp);
     }
+
+    std::swap(ds_node[u - 1].val, ds_node[cha - 1].val);
+    ds_node[u - 1].now = 0;
+    ds_node[u - 1].next = 1;
+
+    ds_node[cha - 1].now = 1;
+    ds_node[cha - 1].next = 0;
+
+    ds_node[cha - 1].per_next = 0;
+    ds_node[u - 1].per_next = 1;
+
+    postition_state.push_back(ds_node);
+    tree_state.push_back(ds_node);
+
+    for(int t = 1; t <= 30 * time; t++) {
+        std::vector<node>tmp;
+        for(int i = 0; i < ds_node.size(); i++) {
+            tmp.push_back(ds_node[i]);
+            if(i == u - 1) {
+                tmp.back().per_next = (30 * time - t) / (30 * time);
+            }
+        }
+        tree_state.push_back(tmp);
+        postition_state.push_back(ds_node);
+    }
+
+    ds_node[u - 1].per_next = 0;
+
+
+
+    postition_state.push_back(ds_node);
+    tree_state.push_back(ds_node);
 }
 
-void HeapScreen::set_position() {
+void HeapScreen::set_ds_node() {
     for(int i = 0; i < ds_node.size(); i++) {
 
         if(ds_node.size() % 2 == 0) {
@@ -338,20 +419,20 @@ float angle(const sf::Vector2f& start, const sf::Vector2f& end) {
     return std::atan2(dy, dx);
 }
 
-void HeapScreen::draw_heap(std::vector<node>& tmp) {
-    draw_node(1, tmp);
+void HeapScreen::draw_heap(std::vector<node>& tree_stage, std::vector<node>& postition_stage) {
+    draw_node(1, tree_stage, postition_stage);
 }
 
 
-void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
-    if(u > ds_node.size()) return;
+void HeapScreen::draw_node(int u, std::vector<node>& tree_state, std::vector<node>& postition_state) {
+    if(u > tree_state.size()) return;
 
     // Vẽ cạnh nối đến con trái
     int v = 2 * u;
-    if(v <= ds_node.size()) {
+    if(v <= tree_state.size()) {
 
-        sf::Vector2f start(ds_node[u - 1].pos_x, ds_node[u - 1].pos_y);
-        sf::Vector2f end(ds_node[v - 1].pos_x, ds_node[v - 1].pos_y);
+        sf::Vector2f start(postition_state[u - 1].pos_x, postition_state[u - 1].pos_y);
+        sf::Vector2f end(postition_state[v - 1].pos_x, postition_state[v - 1].pos_y);
     
         // Tạo hình chữ nhật để làm đường thẳng dày hơn
         sf::RectangleShape Line;
@@ -360,7 +441,7 @@ void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
 
         // Thiết lập kích thước và góc cho hình chữ nhật
         Line.setSize(sf::Vector2f(lineLength, thickness));
-        Line.setFillColor(sf::Color(0, 0, 0, ds_node[u - 1].per_edge_l));
+        Line.setFillColor(sf::Color(0, 0, 0, postition_state[u - 1].per_edge_l));
         Line.setOrigin(thickness / 2, 0); // Đặt tâm hình chữ nhật để xoay
 
         // Đặt vị trí và góc cho hình chữ nhật
@@ -369,15 +450,15 @@ void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
 
         mWindow.draw(Line);
 
-        draw_node(v, ds_node);
+        draw_node(v, tree_state, postition_state);
     }
 
     // Vẽ cạnh nối đến con phải
     v = 2 * u + 1;
-    if(v <= ds_node.size()) {
+    if(v <= tree_state.size()) {
 
-        sf::Vector2f start(ds_node[u - 1].pos_x, ds_node[u - 1].pos_y);
-        sf::Vector2f end(ds_node[v - 1].pos_x, ds_node[v - 1].pos_y);
+        sf::Vector2f start(postition_state[u - 1].pos_x, postition_state[u - 1].pos_y);
+        sf::Vector2f end(postition_state[v - 1].pos_x, postition_state[v - 1].pos_y);
     
         // Tạo hình chữ nhật để làm đường thẳng dày hơn
         sf::RectangleShape Line;
@@ -386,7 +467,7 @@ void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
 
         // Thiết lập kích thước và góc cho hình chữ nhật
         Line.setSize(sf::Vector2f(lineLength, thickness));
-        Line.setFillColor(sf::Color(0, 0, 0, ds_node[u - 1].per_edge_r));
+        Line.setFillColor(sf::Color(0, 0, 0, postition_state[u - 1].per_edge_r));
         Line.setOrigin(thickness / 2, 0); // Đặt tâm hình chữ nhật để xoay
 
         // Đặt vị trí và góc cho hình chữ nhật
@@ -395,24 +476,23 @@ void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
 
         mWindow.draw(Line);
 
-        draw_node(v, ds_node);
+        draw_node(v, tree_state, postition_state);
     }
 
 
 
     // Vẽ node hiện tại
     sf::CircleShape circle(15, 100);
-    if(ds_node[u - 1].now == 0) {
-        circle.setFillColor(sf::Color(255, 255, 255, ds_node[u - 1].per_node));
-        circle.setOutlineColor(sf::Color(0, 0, 0, ds_node[u - 1].per_node));
+    if(tree_state[u - 1].now == 0) {
+        circle.setFillColor(sf::Color(255, 255, 255, tree_state[u - 1].per_node));
+        circle.setOutlineColor(sf::Color(0, 0, 0, tree_state[u - 1].per_node));
     }
     else {
-        circle.setFillColor(sf::Color(255, 165, 0, ds_node[u - 1].per_node));
-        circle.setOutlineColor(sf::Color(255, 165, 0, ds_node[u - 1].per_node));
+        circle.setFillColor(sf::Color(255, 165, 0, tree_state[u - 1].per_node));
+        circle.setOutlineColor(sf::Color(255, 165, 0, tree_state[u - 1].per_node));
     }
     circle.setOutlineThickness(3);
-    circle.setPosition(ds_node[u - 1].pos_x - 15, ds_node[u - 1].pos_y - 15);
-    mWindow.draw(circle);
+    circle.setPosition(tree_state[u - 1].pos_x - 15, tree_state[u - 1].pos_y - 15);
 
 
 
@@ -420,38 +500,237 @@ void HeapScreen::draw_node(int u, std::vector<node> &ds_node) {
     //Vẽ giá trị
     sf::Text Value;
     Value.setFont(MyFont.get(FontID::ptsan));
-    Value.setString(std::to_string(ds_node[u - 1].val));
+    Value.setString(std::to_string(tree_state[u - 1].val));
     Value.setCharacterSize(16);
-    if(ds_node[u - 1].now == 0) Value.setFillColor(sf::Color(0, 0, 0, ds_node[u - 1].per_node));
-    else Value.setFillColor(sf::Color(255, 255, 255, ds_node[u - 1].per_node));
+    if(tree_state[u - 1].now == 0) Value.setFillColor(sf::Color(0, 0, 0, tree_state[u - 1].per_node));
+    else {
+        Value.setFillColor(sf::Color(255, 255, 255, tree_state[u - 1].per_node));
+    }
 
     sf::FloatRect textRect = Value.getLocalBounds();
     Value.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top  + textRect.height / 2.0f);
-    Value.setPosition(ds_node[u - 1].pos_x, ds_node[u - 1].pos_y);
+    Value.setPosition(tree_state[u - 1].pos_x, tree_state[u - 1].pos_y);
 
-    mWindow.draw(Value);
 
     //Vẽ ID
     sf::Text ID;
     ID.setFont(MyFont.get(FontID::ptsan));
     ID.setString(std::to_string(u));
     ID.setCharacterSize(16);
-    ID.setFillColor(sf::Color(255, 0, 0, ds_node[u - 1].per_node));
+    ID.setFillColor(sf::Color(255, 0, 0, tree_state[u - 1].per_node));
 
     textRect = ID.getLocalBounds();
     ID.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top  + textRect.height / 2.0f);
-    ID.setPosition(ds_node[u - 1].pos_x, ds_node[u - 1].pos_y + 30);
+    ID.setPosition(tree_state[u - 1].pos_x, tree_state[u - 1].pos_y + 30);
 
-    mWindow.draw(ID);
 
-    if(ds_node[u - 1].next) {    
-        circle.setFillColor(sf::Color(255, 165, 0, 100));
-        circle.setOutlineColor(sf::Color(255, 165, 0, 100));
-        circle.setOutlineThickness(3);
-        circle.setPosition(ds_node[u - 1].pos_x - 15, ds_node[u - 1].pos_y - 15);
-        mWindow.draw(circle);
+    if(tree_state[u - 1].next) { 
+        float ratio = tree_state[u - 1].per_next; 
+        circle.setOutlineColor(sf::Color(ratio * 255, ratio * 165, 0, tree_state[u - 1].per_node));
+        Value.setFillColor(sf::Color(ratio * 255, ratio * 165, 0, tree_state[u - 1].per_node));
     }
+
+    mWindow.draw(circle);
+    mWindow.draw(Value);
+    mWindow.draw(ID);
 
     return;
 
+}
+
+void HeapScreen::extract_max() {
+    if(ds_node.size()) {
+        if(ds_node.size() > 1) animation_swap_node(1, ds_node.size(), 0.5f);
+        delete_node((int)ds_node.size());
+    }
+}
+
+void HeapScreen::delete_node(int u) {
+    time_vi = 0;
+    dfs_inoder(1, u);
+    animation_shift_tree_delete_node(0.5f);
+    down_heapify(1);
+}
+
+void HeapScreen::animation_shift_tree_delete_node(float time) {
+
+
+    //tree_state.clear();
+    //tree_state.push_back(ds_node);
+    //postition_state.push_back(ds_node);
+
+    for(int t = 1; t <= 60 * time; t++) {
+        std::vector<node>tmp;
+        for(int i = 0; i < ds_node.size(); i++) {
+
+
+            float en_pos_x;
+            float en_pos_y;
+
+            if((ds_node.size() - 1) % 2 == 0) {
+                int mid = (ds_node.size() - 1) / 2;
+                if(ds_node[i].vt <= mid) {
+                    en_pos_x = 720 - (mid - ds_node[i].vt) * 34 - 17;
+                }
+                else {
+                    en_pos_x = 720 + (ds_node[i].vt - (mid + 1)) * 34 + 17;
+                }
+            }
+            else {
+                int mid = (ds_node.size() - 1) / 2 + 1;
+                en_pos_x = 720 + (ds_node[i].vt - mid) * 34;
+            }
+
+            int hi = 0;
+            while((1<<hi) <= i + 1) {
+                hi++;
+            }
+
+            en_pos_y = 120 + (hi - 1) * 60; //len = 50, base = 30
+
+            float new_pos_x = ds_node[i].pos_x + (en_pos_x - ds_node[i].pos_x) * 1.0 * t / (60 * time);
+            float new_pos_y = ds_node[i].pos_y + (en_pos_y - ds_node[i].pos_y) * 1.0 * t / (60 * time);
+            tmp.push_back(ds_node[i]);
+            tmp.back().pos_x = new_pos_x;
+            tmp.back().pos_y = new_pos_y;
+
+            if(i == ds_node.size() - 1) {
+
+                tmp.back().pos_x = ds_node[i].pos_x;
+                tmp.back().pos_y = ds_node[i].pos_y;
+
+                int u = i + 1;
+                int cha = u / 2;
+                float new_per_node = 255 - 255 * 1.0 * t / (60 * time);
+                tmp.back().per_node = new_per_node;
+
+                if(cha >= 1) {
+                    if(2 * cha == u) {
+                        float new_per_edge_l = 255 - 255 * 1.0 * t / (60 * time);
+                        tmp[cha - 1].per_edge_l = new_per_edge_l;
+                    }
+                    else {
+                        float new_per_edge_r = 255 - 255 * 1.0 * t / (60 * time);
+                        tmp[cha - 1].per_edge_r = new_per_edge_r;
+                    }
+                }
+            }
+        }
+
+        tree_state.push_back(tmp);
+        postition_state.push_back(tmp);
+    }
+
+
+    set_ds_node_delete();
+    tree_state.push_back(ds_node);
+    postition_state.push_back(ds_node);
+}
+
+void HeapScreen::set_ds_node_delete() {
+    ds_node.pop_back();
+    for(int i = 0; i < ds_node.size(); i++) {
+
+        if(ds_node.size() % 2 == 0) {
+            int mid = ds_node.size() / 2;
+            if(ds_node[i].vt <= mid) {
+                ds_node[i].pos_x = 720 - (mid - ds_node[i].vt) * 34 - 17;
+            }
+            else {
+                ds_node[i].pos_x = 720 + (ds_node[i].vt - (mid + 1)) * 34 + 17;
+            }
+        }
+        else {
+            int mid = ds_node.size() / 2 + 1;
+            ds_node[i].pos_x = 720 + (ds_node[i].vt - mid) * 34;
+        }
+
+        int hi = 0;
+        while((1<<hi) <= i + 1) {
+            hi++;
+        }
+
+        ds_node[i].pos_y = 120 + (hi - 1) * 60; //len = 50, base = 30
+
+        ds_node[i].per_node = 255;
+        if(2 * (i + 1) <= ds_node.size()) {
+            ds_node[i].per_edge_l = 255;
+        }
+        if(2 * (i + 1) + 1 <= ds_node.size()) {
+            ds_node[i].per_edge_r = 255;
+        }
+    }
+}
+
+void HeapScreen::down_heapify(int u) {
+    int L = 2 * u;
+    int R = 2 * u + 1;
+    if(R <= ds_node.size()) {
+        if(ds_node[L - 1].val > ds_node[R - 1].val) {
+            if(ds_node[L - 1].val > ds_node[u - 1].val) {
+                animation_swap_node(L, u, 0.5f);
+                down_heapify(L);
+            }
+        }
+        else {
+            if(ds_node[R - 1].val > ds_node[u - 1].val) {
+                animation_swap_node(R, u, 0.5f);
+                down_heapify(R);
+            }
+        }
+    }
+    else if(L <= ds_node.size()) {
+        if(ds_node[L - 1].val > ds_node[u - 1].val) {
+            animation_swap_node(L, u, 0.5f);
+            down_heapify(L);
+        }
+    }
+    return;
+}
+
+void HeapScreen::update_pos(int vt, int val) {
+
+    if(vt > ds_node.size()) return;
+
+    ds_node[vt - 1].val = val;
+    for(int i = 0; i < ds_node.size(); i++) {
+        ds_node[i].now = ds_node[i].next = 0;
+    }
+    ds_node[vt - 1].now = 1;
+    //tree_state.clear();
+    //postition_state.clear();
+    tree_state.push_back(ds_node);
+    postition_state.push_back(ds_node);
+
+    up_heapify(vt);
+
+    int cur = -1;
+    for(int i = 0; i < ds_node.size(); i++) {
+        if(ds_node[i].now == 1) cur = i + 1;
+    }
+    down_heapify(cur);
+
+    return;
+}
+
+void HeapScreen::delete_pos(int vt) {
+    if(vt > ds_node.size()) return;
+    
+    int val = ds_node[0].val + 1;
+    ds_node[vt - 1].val = val;
+    for(int i = 0; i < ds_node.size(); i++) {
+        ds_node[i].now = ds_node[i].next = 0;
+    }
+    ds_node[vt - 1].now = 1;
+    //tree_state.clear();
+    //postition_state.clear();
+    tree_state.push_back(ds_node);
+    postition_state.push_back(ds_node);
+
+    up_heapify(vt);
+
+    extract_max();
+    
+
+    return;
 }
